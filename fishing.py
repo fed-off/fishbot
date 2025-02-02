f304f7e4b318a231afee0549943538012d542907
Aleksey Fedorov <138257434+fed-off@users.noreply.github.com>
2025-02-02 23:18:37 +0300

    Update fishing

--- fishing.py
+++ fishing.py
@@ -1,12 +1,28 @@
 import time
 
-import config
 import manager
-import check_biting
+import actions
+
 
 def fishing():
+    fish_count = 0
+    fish_lost = 0
+    start_time = time.time()
+    
+    print('Рыбалка началась')
+
     while manager.is_running:
-        if check_biting.is_biting():
-            print("Поклевка! Подсекаем!")
-            
-        time.sleep(config.CHECK_DELAY)
+        print('\t')
+        actions.cast_fishing_rod()
+        if actions.wait_biting():
+            actions.hook()
+            fish_count += 1
+        else:
+            fish_lost += 1
+
+    total_time = time.time() - start_time
+    minutes = int(total_time) // 60
+    seconds = int(total_time) % 60
+
+    print('\nРыбалка завершена')
+    print(f'\nПрошло {minutes} минут и {seconds} секунд \nПоймал {fish_count} рыб\nУпустил {fish_lost} рыб')
