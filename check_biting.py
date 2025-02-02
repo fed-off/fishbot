b2730aea2805fc011f909d34bd2f162ef51f29c1
Aleksey Fedorov <138257434+fed-off@users.noreply.github.com>
2025-02-02 23:17:54 +0300

    Test distance (switch points)

--- check_biting.py
+++ check_biting.py
@@ -1,11 +1,16 @@
+import numpy as np
+
 import config
 import find_bobber
 
 prev_position = None  # Храним последние координаты
 
+def get_distance(point1, point2):
+    return np.linalg.norm(np.array(point2) - np.array(point1))
+
 def is_biting():
     global prev_position  # Используем глобальную переменную для хранения последних координат
-    delta_position = None
+    # delta_position = None                 Устарело
     # Получаем текущие координаты поплавка
     current_position = find_bobber.get_coordinates()
 
@@ -19,23 +24,23 @@ def is_biting():
         prev_position = current_position
         return False
 
-    left_delta_x = abs(current_position[0][0] - prev_position[0][0])
-    left_delta_y = abs(current_position[0][1] - prev_position[0][1])
-    right_delta_x = abs(current_position[1][0] - prev_position[1][0])
-    right_delta_y = abs(current_position[1][1] - prev_position[1][1])
+    # left_delta_x = abs(current_position[0][0] - prev_position[0][0])
+    # left_delta_y = abs(current_position[0][1] - prev_position[0][1])
+    # right_delta_x = abs(current_position[1][0] - prev_position[1][0])
+    # right_delta_y = abs(current_position[1][1] - prev_position[1][1])
 
-    delta_position = [(left_delta_x, left_delta_y), (right_delta_x, right_delta_y)]
+    # delta_position = [(left_delta_x, left_delta_y), (right_delta_x, right_delta_y)]
 
     # Выводим отладочную информацию
-    print(f"Левая {delta_position[0][0], delta_position[0][1]}\nПравая{delta_position[1][0], delta_position[1][1]}\n---")
+    # print(f"Левая {delta_position[0][0], delta_position[0][1]}\nПравая{delta_position[1][0], delta_position[1][1]}\n---")
 
+    distance = get_distance(current_position[0], prev_position[0])
+    print(distance)
     # Обновляем предыдущие координаты перед следующим циклом
     prev_position = current_position
 
     # Условие для определения поклёвки
-    if ((left_delta_x > config.THRESHOLD or right_delta_x > config.THRESHOLD) 
-        or (left_delta_y > config.THRESHOLD or right_delta_y > config.THRESHOLD)):
-        prev_position = None
-        return True
-    
+    # return ((left_delta_x > config.THRESHOLD_POINTS or right_delta_x > config.THRESHOLD_POINTS) 
+    #     or (left_delta_y > config.THRESHOLD_POINTS or right_delta_y > config.THRESHOLD_POINTS))
 
+    return distance > config.THRESHOLD_DISTANCE
